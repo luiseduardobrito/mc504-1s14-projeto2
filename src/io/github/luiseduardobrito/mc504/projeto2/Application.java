6bd63de440b9277709e7e8ef56663e4b1f74495a
Luís Eduardo Brito <luiseduardo14@gmail.com>
2014-04-21 11:41:14 -0300

    comentarios na classe Application

--- src/io/github/luiseduardobrito/mc504/projeto2/Application.java
+++ src/io/github/luiseduardobrito/mc504/projeto2/Application.java
@@ -8,20 +8,24 @@ public class Application {
 	private static final int NUM_PHILOSOPHERS = 5;
 
 	public static void main(String[] args) {
-
+		
+		// Cria e prepara janela de estado
 		Window.createAndShow();
 		
+		// Inicializa lista de talheres com Locks
 		Lock[] chopsticks = new ReentrantLock[NUM_PHILOSOPHERS];
-
+		
+		// Instancia talheres
 		for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
 			chopsticks[i] = new ReentrantLock();
 		}
-
+		
+		// Inicializa lista de filosofos
 		Philosopher[] philosophers = new Philosopher[NUM_PHILOSOPHERS];
-
+		
+		// Instancia filosofos
 		for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-			philosophers[i] = new Philosopher(i, chopsticks[i],
-					chopsticks[(i + 1) % NUM_PHILOSOPHERS]);
+			philosophers[i] = new Philosopher(i, chopsticks[i], chopsticks[(i + 1) % NUM_PHILOSOPHERS]);
 			new Thread(philosophers[i]).start();
 		}
 	}
